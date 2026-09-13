2c6bfbb4597c853223f00a1a418f314e7affe714
Sara Orion <48605915+SaraRose686@users.noreply.github.com>
2026-09-13 11:54:00 -0700

    Add api folder for Spring Boot application

---
+++ code/api/src/main/java/edu/bu/metcs673/bluejay/BluejayApplication.java
@@ -0,0 +1,13 @@
+package edu.bu.metcs673.bluejay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluejayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluejayApplication.class, args);
+	}
+
+}
---
+++ code/api/src/test/java/edu/bu/metcs673/bluejay/BluejayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bu.metcs673.bluejay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluejayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

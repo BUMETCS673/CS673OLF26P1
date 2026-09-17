877da58158ebaed7468c269fa82ba825c0696fc6
lkimleng <limk@hhbrown.com>
2026-09-17 16:36:54 -0400

    Add simple login form

---
+++ code/web/bluejay-webui/src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  errorMessage: string | null = null;
+
+  onSubmit(): void {}
+}
